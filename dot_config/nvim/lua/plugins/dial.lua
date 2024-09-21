b5c11c455ec08a8e6fd1f2c8181c86093782f231
salam <salamatbekboev2008@gmail.com>
2024-09-22 01:52:45 +0900

    add: dial.nvim config for rust, toml, jsonc and modify python, lua

---
+++ dot_config/nvim/lua/plugins/dial.lua
@@ -0,0 +1,68 @@
+---@module "lazy"
+---@module "dial"
+
+---@type LazySpec[]
+return {
+  "monaqa/dial.nvim",
+  opts = function(_, opts)
+    local augend = require("dial.augend")
+    return vim.tbl_deep_extend("force", opts, {
+      dials_by_ft = {
+        rust = "rust",
+        toml = "toml",
+        jsonc = "json",
+      },
+      groups = {
+        json = vim.list_extend(opts.groups.json, {
+          augend.constant.alias.bool,
+        }),
+        lua = vim.list_extend(opts.groups.lua, {
+          augend.paren.alias.lua_str_literal,
+          augend.paren.alias.quote,
+        }),
+        python = vim.list_extend(opts.groups.python, {
+          augend.constant.new({
+            elements = { "and", "or" },
+            word = true,
+            cyclic = true,
+          }),
+          augend.paren.new({
+            patterns = {
+              { "'''", "'''" },
+              { '"""', '"""' },
+            },
+            nested = false,
+            escape_char = [[\]],
+            cyclic = true,
+          }),
+          augend.paren.alias.quote,
+          augend.integer.alias.octal,
+          augend.integer.alias.binary,
+        }),
+        rust = {
+          augend.constant.new({
+            elements = { "let mut", "let" },
+            word = true,
+            cyclic = true,
+          }),
+          augend.constant.new({
+            elements = { "&&", "||" },
+            word = false,
+            cyclic = true,
+          }),
+          augend.constant.alias.bool,
+          augend.integer.alias.decimal,
+          augend.integer.alias.hex,
+          augend.integer.alias.octal,
+          augend.integer.alias.binary,
+          augend.paren.alias.rust_str_literal,
+        },
+        toml = {
+          augend.constant.alias.bool,
+          augend.integer.alias.decimal,
+          augend.semver.alias.semver,
+        },
+      },
+    })
+  end,
+}
