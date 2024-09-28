f9e65fbb7f8ce11ed8f00475c11dc47dd667eaca
salam <salamatbekboev2008@gmail.com>
2024-09-29 02:32:15 +0900

    add: transparent background for dracula color scheme

--- home/dot_config/nvim/lua/plugins/dracula.lua
+++ home/dot_config/nvim/lua/plugins/dracula.lua
@@ -6,7 +6,17 @@ return {
   {
     "Mofiqul/dracula.nvim",
     opts = {
-      transparent_bg = true,
+      overrides = function(colors)
+        return {
+          Normal = { fg = colors.fg, bg = "None" },
+          NormalFloat = { fg = colors.fg, bg = "None" },
+          SignColumn = { bg = "None" },
+          NvimTreeNormal = { fg = colors.fg, bg = "None" },
+          NvimTreeVertSplit = { fg = colors.bg, bg = "None" },
+          NeoTreeNormal = { fg = colors.fg, bg = "None" },
+          NeoTreeNormalNC = { fg = colors.fg, bg = "None" },
+        }
+      end,
     },
   },
 }
