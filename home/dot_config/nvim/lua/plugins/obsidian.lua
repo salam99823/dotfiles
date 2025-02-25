7bf70e4641903a1c6d9820e1543e702f4ad104be
salam <salamatbekboev2008@gmail.com>
2025-02-25 18:57:06 +0900

    feat: add obsidian plugin for nvim

---
+++ home/dot_config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,24 @@
+---@module "lazy"
+---@module "neo-tree"
+
+---@type LazySpec[]
+return {
+  "epwalsh/obsidian.nvim",
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/vaults/personal",
+      },
+      {
+        name = "work",
+        path = "~/vaults/work",
+      },
+    },
+  },
+}
